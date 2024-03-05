ad2e08e69d9042ef3f04fea2566634897993bf9c
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-05 20:26:09 +0300

    create user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email address')
+    telegram_id = models.IntegerField(verbose_name='Telegram')
+    phone_number = models.CharField(max_length=15, verbose_name='telephone number')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
