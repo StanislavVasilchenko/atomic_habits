68614aa30eb21ddc07a89776cd4ee796b7728a0a
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-05 21:14:55 +0300

    create user permission IsOwner

---
+++ users/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    message = 'You not have permission to perform this action'
+
+    def has_object_permission(self, request, view, obj):
+        return obj.id == request.user.id
