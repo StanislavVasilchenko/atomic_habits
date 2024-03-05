489fc148c37f8bc1ce99932ec0808ff88f553276
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-05 20:37:22 +0300

    urls users

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ class User(AbstractUser):
     username = None
 
     email = models.EmailField(unique=True, verbose_name='email address')
-    telegram_id = models.IntegerField(verbose_name='Telegram')
-    phone_number = models.CharField(max_length=15, verbose_name='telephone number')
+    telegram_id = models.IntegerField(verbose_name='Telegram', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, verbose_name='telephone number', null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
