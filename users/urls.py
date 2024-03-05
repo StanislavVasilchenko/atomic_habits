2d8f87b4884aaadc31385177fefd4ee87e6620a0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-05 20:51:38 +0300

    registration user url

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
+from users.views import UserRegisterAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # user
+    path('register/', UserRegisterAPIView.as_view(), name='user_register'),
 ]
