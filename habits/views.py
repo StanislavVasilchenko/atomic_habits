e55ae40678331fb44204806f1501fa500bf502c8
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-06 22:56:27 +0300

    add HabitListAPIView

--- habits/views.py
+++ habits/views.py
@@ -2,13 +2,24 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from habits.models import GoodHabit
+from habits.permissions import IsHabitOwner
 from habits.serializers import HabitSerializer
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
-    queryset = GoodHabit
+    queryset = GoodHabit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class HabitListAPIView(generics.ListCreateAPIView):
+    queryset = GoodHabit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        return queryset
