9a94391108131ff3e14396857280a531a827f8c7
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-06 22:56:39 +0300

    add HabitListAPIView in url

--- habits/urls.py
+++ habits/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import HabitCreateAPIView
+from habits.views import HabitCreateAPIView, HabitListAPIView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     path('create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('', HabitListAPIView.as_view(), name='habit-list'),
 ]
