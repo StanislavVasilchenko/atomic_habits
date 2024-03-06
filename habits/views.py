2e8474734699ca27e66f31455b5fc897344dcab9
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-06 21:28:32 +0300

    create HabitCreateAPIView

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habits.models import GoodHabit
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    queryset = GoodHabit
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
