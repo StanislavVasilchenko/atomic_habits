c10f1022444afcc7e3f0785ff4accc222dc396f6
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-08 19:31:08 +0300

    perform_create & perform_update

--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from habits.models import GoodHabit
 from habits.paginators import HabitsPaginator
@@ -14,7 +15,10 @@ class HabitCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        new_habit = serializer.save(user=self.request.user)
+        if new_habit.nice_habit:
+            new_habit.reward = None
+            new_habit.save()
 
 
 class HabitListAPIView(generics.ListCreateAPIView):
@@ -38,6 +42,15 @@ class HabitUpdateAPIView(generics.UpdateAPIView):
     queryset = GoodHabit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsHabitOwner]
+
+    def perform_update(self, serializer):
+        updated_habit = serializer.save()
+        if updated_habit.related_habit:
+            updated_habit.reward = None
+        if updated_habit.nice_habit:
+            updated_habit.related_habit = None
+        updated_habit.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class HabitDeleteAPIView(generics.DestroyAPIView):
