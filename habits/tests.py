49ca3ace334bdb8e4d1a205126c9a12f65ee86f3
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-09 21:27:30 +0300

    create HabitsTestCase

--- habits/tests.py
+++ habits/tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from django.contrib.auth.models import AnonymousUser
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -141,3 +142,73 @@ class HabitsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(),
                          {'non_field_errors': ['Переиодичность не должна привышать 7 дней']})
+
+    def test_habits_update(self):
+        self.client.force_authenticate(user=self.user)
+
+        data = {
+            'name': 'update_name'
+        }
+
+        response = self.client.patch(reverse('habits:habit-update', args=[self.good_habit.id]),
+                                     data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('name'), 'update_name')
+
+    def test_habits_update_not_owner(self):
+        self.client.force_authenticate(user=self.user_2)
+
+        data = {
+            'name': 'update_name'
+        }
+
+        response = self.client.patch(reverse('habits:habit-update', args=[self.good_habit.id]),
+                                     data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_habits_update_reward_with_related_habit(self):
+        """При добавлении мвязанной привычки поле вознаграждения становится None"""
+        self.client.force_authenticate(user=self.user)
+
+        data = {
+            'related_habit': self.nice_habit.id
+        }
+
+        response = self.client.patch(reverse('habits:habit-update', args=[self.good_habit.id]),
+                                     data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('reward'), None)
+
+    def test_habits_update_good_habit_in_nice_habit(self):
+        """При изменении полезной привычки в приятную связаная привычка становится None"""
+
+        self.client.force_authenticate(user=self.user)
+
+        data = {
+            'nice_habit': True
+        }
+
+        response = self.client.patch(reverse('habits:habit-update', args=[self.good_habit.id]),
+                                     data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('related_habit'), None)
+
+    def test_habits_list(self):
+        """Список не пубдичных привычек"""
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse('habits:habit-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_habits_public_list(self):
+        """Список публичных привычек"""
+        self.client.force_authenticate(user=self.user_2)
+
+        response = self.client.get(reverse('habits:habit-public-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
