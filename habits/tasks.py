8fe537deab132685622b1a72fbd6d8b4c066b3d4
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-09 20:22:29 +0300

    create telegram_notification

---
+++ habits/tasks.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+import requests
+from celery import shared_task
+
+from habits.models import GoodHabit
+from private_keys import TG_TOKEN
+
+
+@shared_task
+def telegram_notification():
+    url = 'https://api.telegram.org/bot'
+    token = TG_TOKEN
+    time_now = datetime.now().time().strftime('%H:%M')
+    date_now = datetime.now().date().strftime('%Y-%m-%d')
+
+    habits = GoodHabit.objects.filter(time=time_now, nice_habit=False, date_of_completion=date_now)
+
+    if habits:
+        for habit in habits:
+            response = requests.post(url=f'{url}{token}/sendMessage',
+                                     data={
+                                         'chat_id': habit.user.telegram_id,
+                                         'text': f'Вам необходимо выполнить свою привычку {habit.name} в {habit.time} '
+                                                 f'Место - {habit.place} '
+                                                 f'Действие - {habit.action}'
+                                     }
+                                     )
+            print(response.json())
+            habit.date_of_completion += timedelta(days=habit.periodicity)
+            habit.save()
