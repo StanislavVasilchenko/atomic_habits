c8f45d23cd0c8a334e2a9d5481da7e39b737bf27
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-13 21:52:58 +0300

    add NiceHabitsValidator

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 
 from habits.models import GoodHabit
-from habits.validators import HabitsTimeToCompleteValidator, RelatedHabitValidator, HabitsValidator, HabitsPeriodicity
+from habits.validators import (HabitsTimeToCompleteValidator, RelatedHabitValidator, HabitsValidator,
+                               HabitsPeriodicity, NiceHabitsValidator)
 
 
 class HabitSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = GoodHabit
         exclude = ['id', 'user']
         validators = [HabitsTimeToCompleteValidator(field='time_to_complete'),
                       RelatedHabitValidator(field='related_habit'),
                       HabitsValidator(field=['related_habit', 'reward']),
-                      HabitsPeriodicity(field='periodicity')]
+                      HabitsPeriodicity(field='periodicity'),
+                      NiceHabitsValidator(field=['nice_habits', 'reward'])]
