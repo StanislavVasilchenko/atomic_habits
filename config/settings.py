9a700a60bfe59478836d1e12b6a2082612fef72b
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-13 21:13:39 +0300

    CORS setting

--- config/settings.py
+++ config/settings.py
@@ -174,3 +174,13 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': timedelta(minutes=1)
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
