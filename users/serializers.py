a9cdcbed9770198b67206b675f2852a919ae835a
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-18 16:44:58 +0300

    remove password fields from UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,8 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = '__all__'
