5cd3aa3103c12d0fa8dd05ecf69efaf7997e7df6
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-21 17:19:43 +0300

    add save_user_telegram_id

--- users/services.py
+++ users/services.py
@@ -1,6 +1,7 @@
 import requests
 
 from private_keys import TG_TOKEN
+from users.models import User
 
 
 def get_telegram_id():
@@ -8,3 +9,8 @@ def get_telegram_id():
     token = TG_TOKEN
     response = requests.get(url=f'{url}{token}/getUpdates')
     return response
+
+
+def save_user_telegram_id(user: User, user_id: str) -> None:
+    user.telegram_id = user_id
+    user.save()
